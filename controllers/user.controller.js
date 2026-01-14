ea3b169c20a6b97b4e963b41e93cb713d329c26f
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2026-01-14 12:42:38 +0530

    Logout done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -94,8 +94,20 @@ const login = async (req,res) =>{
 } 
 
 const logout = (req,res) =>{
+
+    res.cookie("token",null,{
+        secure:true,
+        maxAge:0,
+        httpOnly:true
+    });
+
+    res.status(200).json({
+        success:true,
+        message:"User logged out successfully"
+    })
     
-}
+
+};
 
 
 
