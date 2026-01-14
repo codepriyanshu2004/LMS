1e3e412c6d0656aed7eb87288c6018228abfdc63
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2026-01-14 12:06:11 +0530

    User Registration Flow

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,59 @@
-import User from "../model/user.model"
+import User from "../model/user.model.js"
+import AppError from "../utils/error.util.js";
 
-const register = (req,res) =>{
-
+const cookieOptions = {
+    maxAge: 7*24*60*60*1000, //7 days
+    httpOnly:true,
+    secure:true
 }
+
+const register = async(req,res,next) =>{
+    const {fullName,email,password} = req.body;
+    
+    if(!fullName || !email || !password){
+        return next(new AppError("All fields are required",400));
+
+    }
+
+    const userExist = User.findOne(email);
+
+    if(userExist){
+        return next(new AppError("Email already exist",400));
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        password,
+        avatar:{
+            public_id:email,
+            secure_url:"https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-collection-image-icon-stock-isolated-object-set-symbol-web-137160339.jpg"
+        }
+
+    });
+
+    if(!user){
+        return next(new AppError("User registtration failed, please try again",400));
+    }
+    
+    //TODO: File upload;
+
+    await user.save();
+
+    user.password = undefined;
+
+    const token = await user.generateJWTToken();
+
+    res.cookie("token",token,cookieOptions);
+
+    res.status(201).json({
+        success:true,
+        message:"User registered successfully",
+        user,
+    });
+
+
+};
 
 const login = (req,res) =>{
     
--- model/user.model.js
+++ model/user.model.js
@@ -1,68 +1,90 @@
-import {Schema,model} from "mongoose"
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({
-
-    fullName:{
-        type: "String",
-        required: [true,"Name is required"],
-        minLength: [5, "Name must be at least 5 character"],
-        maxLength: [50, "Name should be less then 50 character"],
-        lowercase:true,
-        trim: true,
-
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [5, "Name must be at least 5 characters"],
+      maxlength: [50, "Name should be less than 50 characters"],
+      lowercase: true,
+      trim: true
     },
 
-    email:{
-        type: "String",
-         required: [true,"Email is required"],
-         lowercase:true,
-         trim:true,
-         unique: true,
-          match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/,"Please fill valid email address"]
-
-
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please fill valid email address"]
     },
 
-    password:{
-        type: "String",
-        required: [true,"Password is required"],
-        minLength: [8, "Password must be at least 8 character"],
-        select: false
-
-
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+      select: false
     },
 
-    avatar:{
-        public_id:{
-            type: "String",
-        },
-        
-        secure_url:{
-            type: "String"
-        }
-
+    avatar: {
+      public_id: {
+        type: String
+      },
+      secure_url: {
+        type: String
+      }
     },
 
-    role:{
-        type: "String",
-        enum: ["USER","ADMIN"],
-        default: "USER"
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER"
     },
 
-    forgotPasswordToken:{
-        type: "String",
+    forgotPasswordToken: {
+      type: String
     },
 
     forgotPasswordExpiry: {
-        type: Date,
-    },
-    
+      type: Date
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
-},{
-    timestamps:true
+
+
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 
-const User = new model("User",userSchema);
+userSchema.methods.generateJWTToken = async function () {
+  return await jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      subscription: this.subscription,
+      role: this.role
+    },
+    process.env.JWT_SECRET,
 
+    {
+      expiresIn: process.env.JWT_EXPIRY
+    }
+
+  );
+};
+
+const User = model("User", userSchema);
 export default User;
