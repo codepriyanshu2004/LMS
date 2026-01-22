4043966917f50e0cb465e614cb538f8c0da31b6c
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2026-01-22 18:00:27 +0530

    Created course model

---
+++ model/course.model.js
@@ -0,0 +1,73 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const courseSchema = new Schema({
+    
+    title:{
+        type:String,
+        required:[true,"Title is required"],
+        minLength:[8,"Title must be atleast 8 characters"],
+        maxLength:[50,"Title should be less than 60 characters"],
+        trim:true,
+
+    },
+    description:{
+        type:String,
+        required:[true,"Descrption is required"],
+        minLength:[8,"Desccption must be atleast 8 characters"],
+        maxLength:[200,"Description should be less than 200 characters"]
+    },
+    category:{
+        type:String,
+        required:[true,"Category is required"],
+    },
+
+    thumbnail:{
+         public_id:{
+                    type:String,
+                    required:true,
+                },
+                secure_url:{
+                    type:String,
+                     required:true,
+
+                }
+
+    },
+
+    lectures: [
+        {
+            title:String,
+            description:String,
+            lecture:{
+                public_id:{
+                    type:String,
+                     required:true,
+                },
+                secure_url:{
+                    type:String,
+                     required:true,
+                }
+            }
+        }
+
+    ],
+
+    numbersOfLectures:{
+        type:Number,
+        default: 0,
+    },
+
+    createdBy:{
+        type:String,
+         required:true,
+    },
+
+
+}, {
+    timesstamps:true
+
+});
+
+const Course = new model("Course",courseSchema);
+
+export default Course;
