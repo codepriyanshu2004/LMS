503f505b18306ea696620580cb8bfa1cbff12db0
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2026-01-27 23:01:04 +0530

    Create course flow

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,5 +1,7 @@
 import Course from "../model/course.model.js"
 import AppError from "../utils/error.util.js";
+import cloudinary from "cloudinary";
+import fs from "fs/promises"
 
 const getAllCourses = async(req,res,next)=>{
  
@@ -7,7 +9,7 @@ const getAllCourses = async(req,res,next)=>{
  
     const courses = await Course.find({}).select("-lectures");
 
-    res.status.json({
+    res.status(200).json({
         success:true,
         message:"All courses",
         courses,
@@ -50,7 +52,67 @@ const getLecturesByCourseId = async(req,res,next)=>{
 
 }
 
+const createCourse = async(req,res,next) =>{
+    const {title,description,category,createdBy} = req.body;
+
+    if(!title || !description || !category || !createdBy){
+
+        return next(new AppError("All fields are required",400))
+    }
+
+    const course = await Course.create({
+        title,
+        description,
+        category,
+        createdBy,
+        thumbnail:{
+            public_id: "Dummy",
+            secure_url: "Dummy"
+        }
+
+
+    });
+
+    if(!course){
+        return next(new AppError("Course could not created, please try again"),500)
+    }
+
+    if(req.file){
+
+        try {
+
+         const result = await cloudinary.v2.uploader.upload(req.file.path,{
+            folder: "lms"
+
+        });
+
+        if(result){
+            course.thumbnail.public_id = result.public_id;
+            course.thumbnail.secure_url = result.secure_url;
+        }
+
+        fs.rm(`uploads/${req.file.filename}`);
+
+            
+        } catch (error) {
+             return next(new AppError(error.message),400)
+        }
+       
+    }
+
+    await course.save();
+
+    res.status(200).json({
+        success:true,
+        message:"Course created successfully",
+        course,
+    });
+
+
+}
+
 export{
     getAllCourses,
-    getLecturesByCourseId
+    getLecturesByCourseId,
+    createCourse
 }
--- routes/course.route.js
+++ routes/course.route.js
@@ -1,10 +1,13 @@
 import Router from "express";
-import { getAllCourses, getLecturesByCourseId } from "../controllers/course.controller.js";
+import { createCourse, getAllCourses, getLecturesByCourseId } from "../controllers/course.controller.js";
 import { isLoggedIn } from "../middleware/jwtAuth.middleware.js";
+import upload from "../middleware/multer.middlware.js";
 
 const router = Router();
 
 router.get("/",getAllCourses);
+router.post("/", upload.single("thumbnail"),createCourse);
+
 
 router.get("/:id",isLoggedIn, getLecturesByCourseId);
 
